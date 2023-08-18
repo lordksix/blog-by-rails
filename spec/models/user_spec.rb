7d41f00e6cf8df3f5ceaa641ef44c970a6828213
lordksix <121689749+lordksix@users.noreply.github.com>
2023-08-18 03:18:20 -0500

    Add like model unit test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe User, type: :model do
 
   it 'returns three most recent posts' do
     user = User.create!(name: 'Jane Doe', post_counter: 0)
-    old_posts = 3.times.map { Post.create!(author: user, title: 'Old post', created_at: 2.day.ago) }
+    3.times.map { Post.create!(author: user, title: 'Old post', created_at: 2.day.ago) }
     new_posts = 3.times.map { Post.create!(author: user, title: 'New post') }
     expect(user.three_most_recent_posts).to eq new_posts.reverse
   end
