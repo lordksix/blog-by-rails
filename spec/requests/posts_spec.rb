d8bfe3e61551b2d5ebe0daadd8d2890a3166c4f0
lordksix <121689749+lordksix@users.noreply.github.com>
2023-08-22 12:19:56 -0500

    Implement request tests for users and posts controllers

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET /index' do
+  describe 'GET post#index' do
     it 'returns http success' do
       get '/posts/index'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe 'GET /show' do
+  describe 'GET post#show' do
     it 'returns http success' do
       get '/posts/show'
       expect(response).to have_http_status(:success)
