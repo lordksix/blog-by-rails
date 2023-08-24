28848759cf0ff6a4f3bcbbcca9b4d84cf6086b4f
lordksix <121689749+lordksix@users.noreply.github.com>
2023-08-24 01:43:18 -0500

    Add new routs for creating posts comments and add likes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :users, only: [:index, :show] do resources :posts, only: [:index, :show] end
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :new, :create, :show] do
+      resources :comments, only: [:new, :create]
+      resources :likes, only: [:create]
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
