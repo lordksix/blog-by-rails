a998fc5f8307675451760f62be21323dc512e15d
tombom <inoulenkon@gmail.com>
2023-08-29 19:30:21 +0530

    Add integration test for post show page

--- spec/views/users/user_index_spec.rb
+++ spec/views/users/user_index_spec.rb
@@ -3,17 +3,17 @@ require 'rails_helper'
 RSpec.describe 'Index page', type: :system do
   before(:each) do
     @user = User.create(
-        name: 'Max',
-        bio: 'Full-stack developer',
-        photo: 'http://localhost/123',
-        post_counter: 0
+      name: 'Max',
+      bio: 'Full-stack developer',
+      photo: 'http://localhost/123',
+      post_counter: 0
     )
 
     @user_two = User.create(
-        name: 'Jim',
-        bio: 'Hiring Manager',
-        photo: 'http://localhost/321',
-        post_counter: 0
+      name: 'Jim',
+      bio: 'Hiring Manager',
+      photo: 'http://localhost/321',
+      post_counter: 0
     )
   end
 
@@ -35,10 +35,10 @@ RSpec.describe 'Index page', type: :system do
       expect(page).to have_content('Max')
       expect(page).to have_content('Full-stack developer')
     end
-    
+
     it 'should have number of users post' do
-        visit users_path
-        expect(page).to have_content('Number of posts: 0')
-      end
+      visit users_path
+      expect(page).to have_content('Number of posts: 0')
+    end
   end
 end
