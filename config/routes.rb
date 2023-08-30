dafbc7a9e66a7d9bbd6e075d7426d31454d4ec7e
lordksix <121689749+lordksix@users.noreply.github.com>
2023-08-30 18:15:29 -0500

    Update views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  root 'users#index'
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :new, :create, :show] do
       resources :comments, only: [:new, :create]
