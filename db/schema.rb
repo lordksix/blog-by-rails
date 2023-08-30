98effa6b8294eecce710e7e488376f0294c28c19
lordksix <121689749+lordksix@users.noreply.github.com>
2023-08-30 17:27:37 -0500

    Update database schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_30_083953) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_30_084953) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,7 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_30_083953) do
     t.string "name"
     t.string "photo"
     t.text "bio"
-    t.integer "post_counter"
+    t.integer "post_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
@@ -56,6 +56,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_30_083953) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.string "first_name"
     t.string "surname"
     t.index ["email"], name: "index_users_on_email", unique: true
