e036f7827915d06a37e24ed6956abcfba275af0e
tombom <inoulenkon@gmail.com>
2023-08-31 13:30:26 +0530

    Add destroy method on posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  authorize_resource
   def index
     @user = User.find_by(id: params[:user_id])
     page = params[:page] || 1
@@ -7,20 +8,17 @@ class PostsController < ApplicationController
     @total_posts = @user.posts
     @total_pages = (@total_posts.count.to_f / per_page).ceil
   end
-
   def show
     @user = User.find_by(id: params[:user_id])
     @post = @user.posts.find(params[:id])
     @current_user = current_user
     @like = Like.new
   end
-
   def create
     post = Post.new(post_params)
     post.author = current_user
     post.comments_counter = 0
     post.likes_counter = 0
-
     if post.save
       flash[:success] = 'post saved successfully'
       redirect_to "/users/#{current_user.id}/posts"
@@ -28,14 +26,16 @@ class PostsController < ApplicationController
       flash.now[:error] = 'error: question could not be saved'
     end
   end
-
   def new
     @post = Post.new
     @current_user = current_user
   end
-
+  def destroy
+    @current_user = current_user
+    @current_user.posts.destroy(params[:id])
+    redirect_to "/users/#{current_user.id}/posts"
+  end
   private
-
   def post_params
     params.require(:post).permit(:title, :text)
   end
