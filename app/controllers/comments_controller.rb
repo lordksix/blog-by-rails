3e699e0ddcf024d9182f18f509183736a84d8069
lordksix <121689749+lordksix@users.noreply.github.com>
2023-08-31 19:45:56 -0500

    Refactor frontend controllers

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,14 @@
 class CommentsController < ApplicationController
   authorize_resource
+  before_action :find_user_post_by_id
+
   def new
     @comment = Comment.new
-    @post = Post.find(params[:post_id])
   end
+
   def create
     comment = Comment.new(comment_params)
-    comment.post = Post.find(params[:post_id])
+    comment.post = @post
     comment.author = current_user
     if comment.save
       flash[:success] = 'Comment saved successfully'
@@ -16,12 +18,19 @@ class CommentsController < ApplicationController
       redirect_to new_comment
     end
   end
+
   def destroy
-    post = Post.find(params[:post_id])
-    post.comments.destroy(params[:id])
+    @post.comments.destroy(params[:id])
     redirect_to "/users/#{params[:user_id]}/posts/#{params[:post_id]}"
   end
+
   private
+
+  def find_user_post_by_id
+    @post = Post.find(params[:post_id])
+    @user = User.find(params[:user_id])
+  end
+
   def comment_params
     params.require(:comment).permit(:text)
   end
